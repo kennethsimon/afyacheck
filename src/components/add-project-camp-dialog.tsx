2c06358388e60330e12baa50c47d92ae48c031ef
Fredy Mgimba <fredgerman30@gmail.com>
2024-06-21 12:57:53 +0300

    fix : closing and opening dialog

--- src/components/add-project-camp-dialog.tsx
+++ src/components/add-project-camp-dialog.tsx
@@ -4,6 +4,7 @@ import { AddForm, AddFormSchema } from "@/components/add-form";
 import AwesomeDrawer from "@/components/drawer";
 import { Button } from "@/components/ui/button";
 import { z } from "zod";
+import { toast } from "sonner";
 
 type AddProjectOrCampDialogProps = {
   type: "project" | "camp";
@@ -16,8 +17,24 @@ export function AddProjectOrCampDialog({ type }: AddProjectOrCampDialogProps) {
     // Handle form submission here...
     // remember the type has be passed in the props as either "project" or "camp"
     console.log(data);
+    toast("Item created successfully.", {
+      description: "You can now open the item.",
+      action: {
+        label: "Open",
+        onClick: () => {
+          console.log("Opening item...");
+        },
+      },
+    });
 
     // Close the drawer
+    setIsOpen(false);
+  };
+
+  const onClose = () => {
+    toast("Item creation cancelled.", {
+      description: "You can close the drawer.",
+    });
     setIsOpen(false);
   };
 
@@ -28,8 +45,9 @@ export function AddProjectOrCampDialog({ type }: AddProjectOrCampDialogProps) {
       isForm={true}
       isOpen={isOpen}
       onClose={() => setIsOpen(false)}
+      closeTrigger={<Button onClick={() => setIsOpen(false)}>Close</Button>}
     >
-      <AddForm onSubmit={onSubmit} />
+      <AddForm onSubmit={onSubmit} onClose={onClose} />
     </AwesomeDrawer>
   );
 }
