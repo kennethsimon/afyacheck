3930d14410274eb5d04bb91b3791c174b16db634
Fredy Mgimba <fredgerman30@gmail.com>
2024-07-28 23:59:39 +0300

    fix : removed table date filters

--- src/components/table-filters/date-filters.tsx
+++ src/components/table-filters/date-filters.tsx
@@ -3,32 +3,10 @@ import { DateRangePicker } from "@/components/table-filters/date-range-picker";
 export function DateFilters() {
   return (
     <div className="grid pb-4 gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-      <DateRangePicker
+      {/* <DateRangePicker
         triggerSize="sm"
         triggerClassName="ml-auto w-56 sm:w-60"
-      />
-      <DateRangePicker
-        triggerSize="sm"
-        triggerClassName="ml-auto w-56 sm:w-60"
-        fromField="dispatchedFrom"
-        toField="dispatchedTo"
-        placeholder="Dispatched Date"
-      />
-      <DateRangePicker
-        triggerSize="sm"
-        triggerClassName="ml-auto w-56 sm:w-60"
-        fromField="completedFrom"
-        toField="completedTo"
-        placeholder="Completed Date"
-      />
-
-      <DateRangePicker
-        triggerSize="sm"
-        triggerClassName="ml-auto w-56 sm:w-60"
-        fromField="refundedFrom"
-        toField="refundedTo"
-        placeholder="Refunded Date"
-      />
+      /> */}
     </div>
   );
 }
